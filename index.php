0df22becf400fd5c6fbb404e9a852af4ece76170
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-12 12:13:10 +0100

    Title

--- index.php
+++ index.php
@@ -2,22 +2,13 @@
 
 <?php get_header();?>
 
-    <div class="blog-masthead">
-      <div class="container">
-        <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Home</a>
-          <a class="blog-nav-item" href="#">New features</a>
-          <a class="blog-nav-item" href="#">Press</a>
-          <a class="blog-nav-item" href="#">New hires</a>
-          <a class="blog-nav-item" href="#">About</a>
-        </nav>
-      </div>
-    </div>
+
 
     <div class="container">
 
       <?php get_template_part('content', get_post_format() );?>
 
-      
+
 
     </div><!-- /.container -->
+    <?php get_footer(); ?>
